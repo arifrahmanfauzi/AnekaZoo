64043f1b5bc4633693ceb55abce0e0cb9b11547d
arifrahmanfauzi <arif.rahman1780@gmail.com>
2022-03-10 21:32:37 +0700

    add sql file

---
+++ anekazoo.sql
@@ -0,0 +1,28 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               8.0.28 - MySQL Community Server - GPL
+-- Server OS:                    Win64
+-- HeidiSQL Version:             11.2.0.6213
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- Dumping data for table anekazoo.animals: ~5 rows (approximately)
+/*!40000 ALTER TABLE `animals` DISABLE KEYS */;
+INSERT INTO `animals` (`id`, `name`, `class`, `legs`) VALUES
+	(4, 'lama', 'mamalia', 4),
+	(9, 'lion', 'mamalia', 4),
+	(16, 'deer', 'mamalia', 4),
+	(21, 'cow', 'mamalia', 4),
+	(30, 'bear', 'mamalia', 4);
+/*!40000 ALTER TABLE `animals` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
